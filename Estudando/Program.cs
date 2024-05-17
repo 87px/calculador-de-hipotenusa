b302f4da0ee0796a4b3ca8b12ef7f8fa84661a07
Victor <we5705988@gmail.com>
2024-05-17 11:34:52 -0300

    Calculador de hipotenusa 1.0

---
+++ Estudando/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Entre o lado A: ");
+        double ladoA = Convert.ToDouble(Console.ReadLine()); // lado A da hipotenusa;
+
+        Console.WriteLine("Entre o lado B: ");
+        double ladoB = Convert.ToDouble(Console.ReadLine()); // lado B da hipotenusa;
+
+        double hipotenusa = Math.Sqrt((ladoA * ladoA) + (ladoB * ladoB)); // Cálculo para descobrir a hipotenusa;
+        Console.WriteLine("A hipotenusa é: " + hipotenusa);
+    }
+}
+
